960ffb8346b82065aba706be6ded607f6374e601
Leslie Annan <leslie.annan@devopsafricalimited.com>
2023-05-20 11:04:53 +0000

    ...

---
+++ app/src/main/java/com/concealpay_android/ui/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.concealpay_android.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.concealpay_android.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
