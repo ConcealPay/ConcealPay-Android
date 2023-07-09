9f76f3e5f33231206dd7b64fe19d0b31384e6b9d
Leslie Annan <leslie.annan@devopsafricalimited.com>
2023-07-09 19:05:52 +0000

    completed enter phone number input

--- app/src/main/java/com/concealpay/android/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/concealpay/android/ui/onboarding/OnBoardingActivity.kt
@@ -3,12 +3,18 @@ package com.concealpay.android.ui.onboarding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.concealpay.android.R
+import com.concealpay.android.databinding.ActivityOnBoardingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnBoardingBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+
+        binding = ActivityOnBoardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
